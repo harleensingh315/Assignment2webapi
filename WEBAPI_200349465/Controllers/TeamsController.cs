dceb62651c14e1334b25c69134f260164a3c93ea
Harleen Singh <harleensingh315@gmail.com>
2018-12-05 09:41:37 -0500

    Assignment 2 Part 3 Commit 2

---
+++ WEBAPI_200349465/Controllers/TeamsController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEBAPI_200349465.Models;
+
+namespace WEBAPI_200349465.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Teams")]
+    public class TeamsController : Controller
+    {
+        private readonly students_dbContext _context;
+
+        public TeamsController(students_dbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Teams
+        [HttpGet]
+        public IEnumerable<Teams> GetTeams()
+        {
+            return _context.Teams;
+        }
+
+        // GET: api/Teams/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTeams([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var teams = await _context.Teams.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (teams == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(teams);
+        }
+
+        // PUT: api/Teams/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeams([FromRoute] int id, [FromBody] Teams teams)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != teams.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(teams).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TeamsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Teams
+        [HttpPost]
+        public async Task<IActionResult> PostTeams([FromBody] Teams teams)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Teams.Add(teams);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTeams", new { id = teams.Id }, teams);
+        }
+
+        // DELETE: api/Teams/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTeams([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var teams = await _context.Teams.SingleOrDefaultAsync(m => m.Id == id);
+            if (teams == null)
+            {
+                return NotFound();
+            }
+
+            _context.Teams.Remove(teams);
+            await _context.SaveChangesAsync();
+
+            return Ok(teams);
+        }
+
+        private bool TeamsExists(int id)
+        {
+            return _context.Teams.Any(e => e.Id == id);
+        }
+    }
+}
